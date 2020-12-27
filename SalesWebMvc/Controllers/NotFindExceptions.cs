7ca9f5022175a44b70ce361fc05e8f9aa77e40ea
Vinicius Tassi <viniciustassi@gmail.com>
2020-12-27 11:30:10 -0300

    seller edit

---
+++ SalesWebMvc/Controllers/NotFindExceptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SalesWebMvc.Controllers
+{
+    [Serializable]
+    internal class NotFindExceptions : Exception
+    {
+        public NotFindExceptions()
+        {
+        }
+
+        public NotFindExceptions(string message) : base(message)
+        {
+        }
+
+        public NotFindExceptions(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NotFindExceptions(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
